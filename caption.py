41ea73ee27e9008521db348441f69f1498a09201
RpiUnoo <67361365+RpiUnoo@users.noreply.github.com>
2023-10-02 11:11:35 +0530

    Update caption.py

--- caption.py
+++ caption.py
@@ -18,7 +18,7 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 from keras.layers import Input,Dense,Dropout,Embedding,LSTM
 from keras.layers import add
 
-model = load_model("model_9.h5")
+model = load_model("static/model_9.h5")
 model.make_predict_function()
 model_temp = ResNet50(weights='imagenet',input_shape=(224,224,3))
 model_resnet = Model(model_temp.input,model_temp.layers[-2].output)
